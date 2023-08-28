4f391394061df565a8f6d63005379108c88e143a
Pedro Barros <pedro.de.barros.alves@gmail.com>
2023-08-28 11:14:39 -0300

    atualizacao teste criacao relogio

--- Relogio/src/TesteRelogio.java
+++ Relogio/src/TesteRelogio.java
@@ -9,12 +9,11 @@ public class TesteRelogio {
         
 
         relogio1 = new Relogio(10, 15, 5);
-        
-        
-        relogio1.hora.assertEquals(10);
-        relogio1.minuto.assertEquals(15);
-        relogio1.segundo.assertEquals(5);
-        
+
+        if (relogio1.hora.assertEquals(10) == false || relogio1.minuto.assertEquals(15) == false || relogio1.segundo.assertEquals(5) == false) {
+            System.out.println("Teste falho. O relogio foi criado errado/nao foi criado.");
+        }
+
     }
 
 }
