fe4a683654a13559c4128708fc2dd1020dc6ec82
Pedro Barros <pedro.de.barros.alves@gmail.com>
2023-08-28 11:34:26 -0300

    atualizacao teste e criacao relogio

---
+++ Relogio/src/Relogio.java
@@ -0,0 +1,38 @@
+public class Relogio {
+    private int hora, minuto, segundo;
+
+    public Relogio(int hora, int minuto, int segundo) {
+        
+        this.hora = hora;
+        this.minuto = minuto;
+        this.segundo = segundo;
+
+
+
+    }
+
+    public void setHora(int hora){
+        this.hora = hora;
+    }
+
+    public void setMinuto(int minuto){
+        this.minuto = minuto;
+    }
+    
+    public void setSegundo(int segundo){
+        this.segundo = segundo;
+    }
+    
+    
+    public int getHora(){
+        return hora;
+    }
+
+    public int getMinuto(){
+        return minuto;
+    }
+
+    public int getSegundo(){
+        return segundo;
+    }
+}
--- Relogio/src/TesteRelogio.java
+++ Relogio/src/TesteRelogio.java
@@ -7,12 +7,20 @@ public class TesteRelogio {
     @Test
     public void teste() {
         
+        
 
-        relogio1 = new Relogio(10, 15, 5);
+        
 
-        if (relogio1.hora.assertEquals(10) == false || relogio1.minuto.assertEquals(15) == false || relogio1.segundo.assertEquals(5) == false) {
-            System.out.println("Teste falho. O relogio foi criado errado/nao foi criado.");
-        }
+        Relogio relogio1 = new Relogio(10, 15, 5);
+
+        int hora = relogio1.getHora();
+        int minuto = relogio1.getMinuto();
+        int segundo = relogio1.getSegundo();
+
+        assertEquals(10, hora);
+        assertEquals(15, minuto);
+        assertEquals(5, segundo);
+        
 
     }
 
